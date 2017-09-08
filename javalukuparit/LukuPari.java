39ec64be70aae7a50108741e380f0dca106ecca5
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-09-08 15:09:40 +0300

    Add files via upload

--- javalukuparit/LukuPari.java
+++ javalukuparit/LukuPari.java
@@ -1,33 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javalukuparit;
-
-/**
- *
- * @author sakari
- */
 public class LukuPari {
+    
+//nämä ovet luokan attribuutit    
 int luku1;
 int luku2;
+String merkkijono;
+ 
+//tämä on konstrukori, eli tämä alustaa LukuPari objektin luvilla a ja , joka käynnistetään LukuPariTest luokasta
+LukuPari(int a, int b) {
+    this.setLuku1(a);
+    this.setLuku2(b);
+ }
 
-LukuPariTesti testi = new LukuPariTesti();
-
+//nämä metodit alustavat attribuutin niihin syötetyillä arvoilla
  // Setter
   public void setLuku1(int c) {
     this.luku1 = c;
   }
-  
   // Setter
   public void setLuku2(int c) {
     this.luku2 = c;
   }
   
-  public void LukuPari(int a, int b) {
-    this.setLuku1(a);
-    this.setLuku2(b);
+  //näillä metodeilla luetaan attribuutien arvot
+   // Getter
+  public int getLuku1() {
+    return luku1;
   }
+  
+  // Getter
+  public int getLuku2() {
+    return luku2;
+  }
+  
+  //Tämä metodi vain laittaa merkkijonot peräkanaa
+  public String getString (){
+      merkkijono = "luku 1 on " + Integer.toString(luku1) + " ," + "luku 2 on " + Integer.toString(luku2);
+      return merkkijono;
+  } 
     
 }
--- javalukuparit/LukuPariTesti.java
+++ javalukuparit/LukuPariTesti.java
@@ -1,40 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javalukuparit;
 import java.util.Scanner;
-
-/**
- *
- * @author sakar
- */
 public class LukuPariTesti {
      
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+    // luetaan kaski kokonaislukua: luku1 ja luku2
     Scanner input = new Scanner(System.in);
     System.out.println("lue 1.kokonaisluku");
     int luku1 = input.nextInt();
     input.nextLine();
     System.out.println("lue 2.kokonaisluku");
     int luku2 = input.nextInt();
+   
+    //luvut annetaan ja syötetään LukuPari attribuuteiksi suoraan LukuPari oliota luotaessa. Luvut ovat
+    //konstruktorin parametreina
+    LukuPari pari1 = new LukuPari(luku1,luku2);
     
-    LukuPari pari1 = new LukuPari();
-    pari1.setLuku1(luku1);
-    pari1.setLuku2(luku2);
+    //TAI tämä on toinen vaihtoehto
+    //************************************************************************************
+      // pari1 on LukuPari-olion instanssimuuttuja
+    // luku1 ja luku2 viedään viedään LukuPari:n objektille instanssimuutujiin
     
-    LukuPari pari2 = new LukuPari(3,9);
+    //LukuPari pari1 = new LukuPari();
+    //pari1.setLuku1(luku1);
+    //pari1.setLuku2(luku2);
+    //***********************************************************************************'
     
-    System.out.println("luku 1 on " +pari1.luku1);
-    System.out.println("luku 2 on " +pari1.luku2);
+    //luvut 1 ja 2 haetaan getLuku metodilla, johon päästään luodun pari1 objektin kautta
+    System.out.println("luku 1 on " +pari1.getLuku1());
+    System.out.println("luku 2 on " +pari1.getLuku2());
     
-    System.out.println(pari1.toString());
-    System.out.println(pari1);
+    System.out.println(pari1.getString());
     
     }
-    
 }
