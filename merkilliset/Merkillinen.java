7190796c0c88ec9789c4cebf47724b69c78fa657
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-09-11 15:34:44 +0300

    Add files via upload

--- merkilliset/Merkillinen.java
+++ merkilliset/Merkillinen.java
@@ -4,20 +4,47 @@ import java.util.Scanner;
 
 public class Merkillinen {
 
-    static String merkit = "*";
-    static String merkkijono="";
+    static String merkit = "*"; // tämä oli oletusmerkki
+    static String merkkijono = ""; // aloitetaan aina tyhjällä merkkijonolla
+    Scanner input = new Scanner(System.in);
+    
+    //Tämä on "setter". this viittaa tähän omaan instanssiin (eli objektiin)
     public void setString(String a){
-        this.merkit=a;
+       this.merkit = a;
     }
     
+    //Tehtävä 27 Merkillinen rivi. Tässä tehdään rivi
     void teeRivi(int pituus){
-    Scanner input = new Scanner(System.in);
     System.out.println("tulosta valittu merkki valitun määrän kertoja:" );
     input.nextLine();
+    for(int x = 1; x <=pituus; x++) {
+    merkkijono = merkkijono + merkit; //String merkkijonot voidaan yhdistää "+" merkillä
+    System.out.println(merkkijono);
+    }
+    }
     
-    for(int x = 1; x <=pituus; x++)
+    //Tehtävä 28 Merkillinen neliö. Tässä tehdään neliö
+     void teeNeliö(int korkeus, int leveys){
+    System.out.println("tulosta neliö (korkeus, leveys):" );
+      
+    // tarvitaan kaksi for luuppia. Ensimmäisellä tehdään rivi-merkkijono, 
+    for(int x = 1; x <=leveys; x++){
+    merkkijono = merkkijono + merkit;
+    }
+    //...ja toisella luupilla toistetaan merkkojoa allekkain  
+    for(int y = 1; y <=korkeus; y++){
+    System.out.println(merkkijono);
+    }
+    }
+    
+    //Tehtävä 29 Merkillinen kolmio. Tässä tehdään kolmio
+    void teeKolmio(int korkeus){
+    System.out.println("tulosta kolmio:" );
+    for(int y=1; y<=korkeus; y++){   
+    for(int x = 1; x <=korkeus; x++){
     merkkijono = merkkijono + merkit;
     System.out.println(merkkijono);
-    
     }
+    }
+    } 
 }
--- merkilliset/MerkillinenTesti.java
+++ merkilliset/MerkillinenTesti.java
@@ -2,24 +2,49 @@ package merkilliset;
 import java.util.Scanner;
 
 public class MerkillinenTesti {
+    int valinta;
     public static void main(String[] args) {
     
     Merkillinen merkillinen = new Merkillinen();
     Scanner input = new Scanner(System.in);
+     
+     // luetaan yksi merkki
+        System.out.println("lue yksi merkki");
+        String merkki = input.next();
+        merkillinen.setString(merkki);
+        input.nextLine();
 
-    //luetaan kuinka monta kertaa merkki tulostetaan
-    System.out.println("lue kuinka monta kertaa merkki tulostetaan");
-    int toistokerrat = input.nextInt();
-
-    // luetaan yksi merkki
-    System.out.println("lue yksi merkki");
-    String merkki = input.next();
-    input.nextLine();
-    
-    
-    merkillinen.setString(merkki);
-    merkillinen.teeRivi(toistokerrat);
+        //1 = rivi, 2 = neliö, 3 = kolmio, muu = poistu
+        System.out.println("1 = rivi, 2 = neliö, 3 = kolmio, muu numero = poistu");
+        int valinta = input.nextInt();
+        input.nextLine();
+      
+    switch(valinta) {
         
+        case 1://1. ensimmäinen valinta tekee tämän
+            
+        System.out.println("lue kuinka monta kertaa merkki tulostetaan");
+        int toistokerrat = input.nextInt();
+        merkillinen.teeRivi(toistokerrat);
+        break;
+        
+        case 2://2. toinen valinta tekee tämän
+        
+        System.out.println("Tee neliö: lue korkeus");
+        int korkeus = input.nextInt();
+        input.nextLine();
+        System.out.println("Tee neliö: lue leveys");
+        int leveys = input.nextInt();
+        merkillinen.teeNeliö(korkeus, leveys);  
+        break;
+        
+        case 3: //3. valinta tekee tämän, muut valinnat lopettaa 
+        System.out.println("Tee kolmio: lue korkeus");
+        int korkeusKolmio = input.nextInt();
+        input.nextLine();
+        merkillinen.teeKolmio(korkeusKolmio); 
+    }
+    
     }
     
     }
