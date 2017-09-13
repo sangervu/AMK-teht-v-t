028547c8292503886999fc9b39f35e49df2f125b
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-09-13 19:14:15 +0300

    Add files via upload

--- anturit/Anturi.java
+++ anturit/Anturi.java
@@ -3,14 +3,20 @@ package anturit;
 
 public class Anturi {
     
-   double  lämpötila;
+   static double lämpötila = 0; // alustetaan lämpötila niin, että while loop lähtee käyntiin
      
     public double getLämpötila(){
-        
+     
+    while (lämpötila < 10. || lämpötila > 30.) {    
     // tähän satunnaismuuttuja, joka generoi lämpötilan
     // testaa lämpötilä if lausekkeella, että se on annetussa välissä
-        
-    return lämpötila;
+    lämpötila = Math.round(Math.random()*50);
+    // random antaa lukuja välillä [0,1]
+    // nyt lämpötila saa lukuja välillä [0,50]. ja näistä pitää valita luvut väliltä [10.0,30.0]
     }
     
+    // nyt palautuu vain arvoja välillä [10.0,30.0]
+    return lämpötila;
+    
+    }
 }
--- anturit/Testi.java
+++ anturit/Testi.java
@@ -1,17 +1,25 @@
 
 package anturit;
 
+import java.util.Scanner;
+
 public class Testi {
 
-double lämpötila;
+static double lämpötila;
+static double valinta = 1;
 
     public static void main(String[] args) {
     
-    Anturi anturi = new Anturi(); // tehdään anturista objekti
-    lämpötila = anturi.getLämpötila();
+    Scanner lukija = new Scanner(System.in);
     
-    // lisää joku toiminne, jolla aktivoit lämpötilan mittaamisen (esim. syötä jotakin, niin silloin lämpötilan mittaus käynnistyy)
-    System.out.println(lämpötila);
+    while (valinta == 1){
+        System.out.println("Mittaa lämpötila: anna 1, Lopeta mittaaminen: annan 0");
+        valinta = lukija.nextInt();    
+        Anturi anturi = new Anturi(); // tehdään anturista objekti
+        lämpötila = anturi.getLämpötila(); // anturi objetilla käynnistetään getLämpötila metodi
+        // lisää joku toiminne, jolla aktivoit lämpötilan mittaamisen (esim. syötä jotakin, niin silloin lämpötilan mittaus käynnistyy)
+        System.out.println(lämpötila +" C");
+        }
+        System.out.println("Mittaus lopetettu");
     }
-    
 }
