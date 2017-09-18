e1bce444ea1026c369d01b9a5c2b3ffbedc49079
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-09-18 22:16:50 +0300

    Add files via upload

--- pankkitili/MaksuKortti.java
+++ pankkitili/MaksuKortti.java
@@ -1,16 +1,33 @@
-
 package pankkitili;
 
 public class MaksuKortti {
-    
-private int PIN;
 
-  public void setPin(int pin) {
-  if(pin>=0 && pin<=9999){    
-  PIN = pin;
-  }
-  else{ 
-  System.out.println("Väärä PIN");    
-  }
-  }
+    private int PIN;
+    private int ekaPin;
+    private int tokaPin;
+
+    TiliTesti testi = new TiliTesti();
+
+    public void setPinEka(int pin) {
+        if (pin >= 0 && pin <= 9999) {
+            ekaPin = pin;
+        } else {
+            System.out.println("Anna PIN uudestaan");
+        }
+    }
+
+    public void setPinToka(int pin) {
+        if (pin >= 0 && pin <= 9999) {
+            tokaPin = pin;
+        } else {
+            System.out.println("Anna PIN uudestaan");
+        }
+        if (ekaPin == tokaPin) {
+            PIN = tokaPin;
+            testi.pinOk(true);
+        } else {
+            testi.pinOk(false);
+        }
+    }
 }
+ 
--- pankkitili/TiliTesti.java
+++ pankkitili/TiliTesti.java
@@ -3,48 +3,50 @@ package pankkitili;
 import java.util.Scanner;
 
 public class TiliTesti {
-    
-    public static void main(String[] args) {
-    int valinta = 1;  
-    Scanner lukija = new Scanner(System.in);
-    PankkiTili tili = new PankkiTili();
-    MaksuKortti kortti = new MaksuKortti();
-    
-    while (valinta != 0){
-    System.out.println("1 = Laita rahaa tilille, 2 = Nosta rahaa tililtä, 3 = Tulosta tilin saldo, 4 = Anna uusi PIN, 0 = Lopetus");
-    valinta = lukija.nextInt();
-    
-    if (valinta== 1){
-        System.out.println("Anna syötettävä määrä €"); 
-        double panoEuro = lukija.nextInt();
-        tili.pano(panoEuro);
-        }
-        else if (valinta==2){
-        System.out.println("Anna nostettava määrä €"); 
-        double nostoEuro = lukija.nextInt();
-        tili.otto(nostoEuro);
 
+    public static void main(String[] args) {
+        int valinta = 1;
+        Scanner lukija = new Scanner(System.in);
+        PankkiTili tili = new PankkiTili();
+        MaksuKortti kortti = new MaksuKortti();
+
+        while (valinta != 0) {
+            System.out.println("1 = Laita rahaa tilille, 2 = Nosta rahaa tililtä, 3 = Tulosta tilin saldo, 4 = Anna uusi PIN, 0 = Lopetus");
+            valinta = lukija.nextInt();
+
+            if (valinta == 1) {
+                System.out.println("Anna syötettävä määrä €");
+                double panoEuro = lukija.nextInt();
+                tili.pano(panoEuro);
+            } else if (valinta == 2) {
+                System.out.println("Anna nostettava määrä €");
+                double nostoEuro = lukija.nextInt();
+                tili.otto(nostoEuro);
+
+            } else if (valinta == 3) {
+                double saldo = tili.getSaldo();
+                System.out.println("Anna saldo = " + saldo + " €");
+
+            } else if (valinta == 4) {
+                System.out.println("Syötä uusi PIN koodi");
+                int pinEka = lukija.nextInt();
+                kortti.setPinEka(pinEka);
+                System.out.println("Syötä uusi PIN koodi uudestaan");
+                int pinToka = lukija.nextInt();
+                kortti.setPinToka(pinToka);
+
+            } else if (valinta == 0) {
+                System.out.println("Istunto lopetettu");
+
+            } else {
+                System.out.println("Tee uusi valinta");
+            }
         }
-        else if (valinta==3){
-        double saldo = tili.getSaldo();
-        System.out.println("Anna saldo = "+saldo+" €" );
-        
-        }
-        
-        else if (valinta==4) {
-        System.out.println("Syötä uusi PIN koodi");
-        int pin = lukija.nextInt();
-        kortti.setPin(pin);
-        }
-        
-        else if (valinta==0) {
-        System.out.println("Istunto lopetettu");
-    
-        }
-        
-        
-        else
-        System.out.println("Tee uusi valinta");
     }
-}
+
+    public void pinOk(boolean test) {
+        boolean pinTest = test;
+        System.out.println("PIN koodi  " + pinTest);
+
+    }
 }
