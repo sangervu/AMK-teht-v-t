7d0177ca023fb93dc746052f0d6e018f4471af4e
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-09-20 11:58:18 +0300

    Add files via upload

---
+++ lentohorisontti/LentoHorisontti.java
@@ -0,0 +1,47 @@
+package lentohorisontti;
+
+import java.util.Scanner;
+
+public class LentoHorisontti {
+
+    private static final double radiusEarth = 6371000; // maapallon säde [m]
+    
+    
+    public static void main(String[] args) {
+
+        double lentoKorkeus; //varataan lentokorkeudelle parametri metreinä
+        double lentoNopeusKh; //varataan lentonopeudelle parametri kilometriä tunnissa
+        double lentoNopeusMs; //varataan lentonopeudelle parametri metriä sekunnissa
+        double alfaRad; // varataan parametri kulmalle maapallom keskipisteessä radiaaneina
+        double aika; // varataan parametri ajalle, jonka lentokoen on näkyvissä
+        double lentokoneenKulkemaIlmaMatkaM; //varataan parametri lentokoneen kulkemalle ilmamatkalle [m]
+        double lentokoneenKulkemaMaaMatkaM; //varataan parametri lentokoneen kulkemalle maamatkalle [m]
+        double lentokoneenKulkemaMaaMatkaKm; //varataan parametri lentokoneen kulkemalle maamatkalle [Km]
+       
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Anna lentokoneen lentokorkeus [m]");
+        lentoKorkeus = input.nextDouble();
+        
+        System.out.println("Anna lentokoneen maanopeus [km/h]");
+        lentoNopeusKh = input.nextDouble();
+        lentoNopeusMs = lentoNopeusKh * 1000 / 3600; //sama kuin yllä, mutta [m/s]
+
+        alfaRad = Math.acos(radiusEarth / (radiusEarth + lentoKorkeus));
+
+        /* nämä on vain testaus varten
+        System.out.println("alfaRad = " + alfaRad);
+        System.out.println("lentokoneen kulkema matka [m] = " + lentokoneenKulkemaMatka);
+        System.out.println("lentokoneen maanopeus [m/s] = " + lentoNopeusMs);
+         */
+        lentokoneenKulkemaIlmaMatkaM = alfaRad * (radiusEarth + lentoKorkeus); // metriessä
+        lentokoneenKulkemaMaaMatkaM = alfaRad * (radiusEarth); // metreissä
+        lentokoneenKulkemaMaaMatkaKm = Math.round(lentokoneenKulkemaMaaMatkaM / 1000);
+
+        aika = lentokoneenKulkemaIlmaMatkaM / lentoNopeusMs;
+        System.out.println("Aika, jonka lentokone näkyy ennen kuin häviää horisonttiin = " + Math.round(aika) + " sekuntia");//kökkö tapa esittää aika!
+        
+        System.out.println("Matka, jonka lentokone etenee maahan nähden ennen kuin häviää horisontin taakse = "
+                + lentokoneenKulkemaMaaMatkaKm + " km");
+    }
+}
