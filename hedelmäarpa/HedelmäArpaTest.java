71848fc3a88b98e8c986478eb90599d1558ade7b
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-10-10 15:49:50 +0300

    Add files via upload

--- hedelmäarpa/HedelmäArpaTest.java
+++ hedelmäarpa/HedelmäArpaTest.java
@@ -12,8 +12,9 @@ public class HedelmäArpaTest {
 
         //int arvonnat = 0;
         int jatkaLoopissa = 1;
-        int lukitus1;
-        int lukitus2;
+        int lukitse;
+        int lukitusValinta;
+        char lukko = ' '; //varataan lukko muistipaikka, joka on alustettu yhdellä tyhjällä merkillä
 
         Scanner input = new Scanner(System.in);
 
@@ -21,38 +22,54 @@ public class HedelmäArpaTest {
 
             System.out.println("Anna tripletti: = 1, Lopeta: = muu valinta");
             jatkaLoopissa = input.nextInt();
-            peliTulos = "";
-            for (int a = 1; a <= 3; a++) {
 
-                HedelmäArpa.hedelmaArpa();
-                arpa = HedelmäArpa.arpa;
-                //intArpa = HedelmäArpa.intArpa;
-                peliTulos = peliTulos + arpa;
+            if (jatkaLoopissa == 1) { // hypätään tämän yli jos valinta on "Lopeta"
+                peliTulos = "";
+                for (int a = 1; a <= 3; a++) {
 
-            }
-            System.out.println("hedelmäpelin tulos: " + peliTulos);
-            System.out.println("haluatko lukita EI = 1, KYLLÄ = 2 ");
-            lukitus1 = input.nextInt();
-            if (lukitus1 == 2) {
-                System.out.println("lukitse 1. =  1, lukitse 2. = 2, lukitse 3. = 3");
-                lukitus2 = input.nextInt();
-
-                switch (lukitus2) {
-                    case 1:
-
-                        break;
-
-                    case 2:
-
-                        break;
-
-                    case 3:
-
-                        break;
+                    HedelmäArpa.hedelmaArpa();
+                    arpa = HedelmäArpa.arpa;
+                    //intArpa = HedelmäArpa.intArpa;
+                    peliTulos = peliTulos + arpa;
 
                 }
-            }
+                System.out.println("hedelmäpelin tulos: " + peliTulos);
+                System.out.println("haluatko lukita EI = 1, KYLLÄ = 2 ");
+                lukitse = input.nextInt();
+                if (lukitse == 2) {
+                    System.out.println("lukitse 1. =  1, lukitse 2. = 2, lukitse 3. = 3");
+                    lukitusValinta = input.nextInt();
 
+                    switch (lukitusValinta) {
+                        case 1: // ensmimmäinen merkki lukitaan
+
+                            lukko = peliTulos.charAt(lukitusValinta - 1); // kirjotetaan ensimmäinen merkki muistiin muistipaikaan lukko.
+                            //lukko sisältää vain yhden char merkin, joka on ensimmäinen hedelmä tripletissä
+
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 2
+                            String arpa1 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa1" String muuttujaan
+
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 3
+                            String arpa2 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa2" String muuttujaan
+
+                            peliTulos = lukko + arpa1 + arpa2; // summataan muistipaikkojen lukko, arpa1 ja arpa2 tulokset yhteen.
+                            //Jokaisessa muistipaikassa on vain vain character muistissa, jolloin summassa on kolme char merkkiä
+                            System.out.println("hedelmäpelin tulos: " + peliTulos); //tulostetaan uusi pelitulos, jossa ensimmäinen merkkki oli lukittu
+
+                            break;
+
+                        case 2: // toinen merkki lukitaan
+
+                            break;
+
+                        case 3: // kolmas merkki lukitaan
+
+                            break;
+
+                    }
+                }
+
+            }
         }
 
     }
