1d8ca42d8c40cc1b828e442ff704e9f9fbb2d901
Sakari Angervuori <sakari.angervuori@kotiposti.net>
2017-10-10 21:54:40 +0300

    Add files via upload

--- hedelmäarpa/HedelmäArpaTest.java
+++ hedelmäarpa/HedelmäArpaTest.java
@@ -59,10 +59,34 @@ public class HedelmäArpaTest {
                             break;
 
                         case 2: // toinen merkki lukitaan
+                            lukko = peliTulos.charAt(lukitusValinta - 1); // kirjotetaan ensimmäinen merkki muistiin muistipaikaan lukko.
+                            //lukko sisältää vain yhden char merkin, joka on ensimmäinen hedelmä tripletissä
 
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 2
+                            String arpa3 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa1" String muuttujaan
+
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 3
+                            String arpa4 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa2" String muuttujaan
+
+                            peliTulos = arpa3 + lukko + arpa4; // summataan muistipaikkojen lukko, arpa1 ja arpa2 tulokset yhteen.
+                            //Jokaisessa muistipaikassa on vain vain character muistissa, jolloin summassa on kolme char merkkiä
+                            System.out.println("hedelmäpelin tulos: " + peliTulos); //tulostetaan uusi pelitulos, jossa ensimmäinen merkkki oli lukittu
                             break;
 
                         case 3: // kolmas merkki lukitaan
+
+                            lukko = peliTulos.charAt(lukitusValinta - 1); // kirjotetaan ensimmäinen merkki muistiin muistipaikaan lukko.
+                            //lukko sisältää vain yhden char merkin, joka on ensimmäinen hedelmä tripletissä
+
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 2
+                            String arpa5 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa1" String muuttujaan
+
+                            HedelmäArpa.hedelmaArpa(); // "heitetään" arpaa merkille 3
+                            String arpa6 = HedelmäArpa.arpa; // sijoitetaan saatu arvan tulos "arpa2" String muuttujaan
+
+                            peliTulos = arpa5 + arpa6 + lukko; // summataan muistipaikkojen lukko, arpa1 ja arpa2 tulokset yhteen.
+                            //Jokaisessa muistipaikassa on vain vain character muistissa, jolloin summassa on kolme char merkkiä
+                            System.out.println("hedelmäpelin tulos: " + peliTulos); //tulostetaan uusi pelitulos, jossa ensimmäinen merkkki oli lukittu
 
                             break;
 
